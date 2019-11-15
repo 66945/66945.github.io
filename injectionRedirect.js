75d833b96526519b77bb4e271285c84f3e3da7fc
66945 <54561572+66945@users.noreply.github.com>
2019-11-15 09:02:31 -0700

    Update injectionRedirect.js

--- injectionRedirect.js
+++ injectionRedirect.js
@@ -1,10 +1,10 @@
 const NINEPM = 18;
 
-redirect();
-
 function redirect() {
     let date = new Date();
 
     if(date.getHours() >= NINEPM)
         location.replace("https://google.com/search?q=cats");
 }
+
+redirect();
