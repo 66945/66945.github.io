d4a082059ba9e4d644eb52071ab7e0fbcdc7f400
66945 <54561572+66945@users.noreply.github.com>
2020-07-24 15:40:25 -0600

    Add files via upload

---
+++ newsgame/headlines.js
@@ -0,0 +1,107 @@
+function collectHeadlines() {
+    $.get(CNN, function(response) {
+        let items = response["items"];
+        console.log(response["items"]);
+
+        for(let i = 0; i < items.length && i < MAX_PER_ORG; i++) {
+            headlines.push([items[i]["title"], 0]);
+            // console.log(items[i]["title"]);
+        }
+
+        setupGame();
+    });
+
+    $.get(FOX, function(response) {
+        let items = response["items"];
+        console.log(response["items"]);
+
+        for(let i = 0; i < items.length && i < MAX_PER_ORG; i++) {
+            headlines.push([items[i]["title"], 1]);
+            // console.log(items[i]["title"]);
+        }
+
+        setupGame();
+    });
+
+    $.get(TIMES, function(response) {
+        let xml = new XMLSerializer().serializeToString(response);
+        // console.log(xml);
+
+        let timesArr = [...xml.matchAll(TIMES_RE)];
+
+        for(let i = 2; i < timesArr.length && i-2 < MAX_PER_ORG; i++) {
+            headlines.push([timesArr[i][1], 2]);
+            console.log(timesArr[i][1]);
+        }
+
+        setupGame();
+    });
+
+    $.get(ONION, function(response) {
+        let items = response["items"];
+        console.log(response["items"]);
+
+        for(let i = 0; i < items.length && i < MAX_PER_ORG; i++) {
+            headlines.push([items[i]["title"], 3]);
+            // console.log(items[i]["title"]);
+        }
+
+        setupGame();
+    });
+}
+
+function collectHeadlinesTest() {
+    headlines = [
+        ["Trump cancels Republican convention activities in Jacksonville", 0],
+        ["Trump says he could send as many as 75,000 federal agents to US cities", 0],
+        ["Fact check: Trump continues to dishonestly downplay the pandemic", 0],
+        ["Colorado 2020 Senate race: What to know about the Gardner-Hickenlooper contest", 1],
+        ["Nancy Pelosi Calls Jamaal Bowman To Scold Him For Winning Primary", 3],
+        ["Barack Obama and Joe Biden Join Forces in Video Targeting Trump", 2],
+        ["CDC rolls out tools for schools to reopen safely during coronavirus outbreak: 'Critically important'", 1],
+        ["A.O.C. Unleashes a Viral Condemnation of Sexism in Congress", 2],
+        ["Kansas City mayor: 'Doxing' of police, officials should be criminalized", 1],
+        ["Self-Loathing GOP Congressman Can’t Believe He’s Been Reduced To Defending Necessity Of Public Schools", 3],
+        ["Timeline Of Presidential Polling", 3],
+        ["Bloomberg's Everytown for Gun Safety Pours $15 Million Into Races in 8 States", 2]
+    ];
+
+    loaded = 3;
+    setupGame();
+}
+
+function shuffleHeadlines() {
+    for(let i = headlines.length - 1; i > 0; i--) {
+        let rand = Math.floor(Math.random() * (i + 1));
+
+        let t = headlines[i];
+        headlines[i] = headlines[rand];
+        headlines[rand] = t;
+    }
+}
+
+function setupGame() {
+    loaded++;
+    
+    if(loaded == 4) {
+        startGame();
+    }
+}
+
+function startGame() {
+    if(currentNum > 0) {
+        $("#false").hide();
+        $("#check").hide();
+        $("#end").hide();
+
+        $("#headline").show();
+    }
+
+    currentNum = 0;
+    shuffleHeadlines();
+
+    current = headlines[currentNum];
+    $("#headline").html(capitalizeWords(current[0]));
+    
+    $(".banner").html("0 / " + headlines.length);
+}
---
+++ newsgame/script.js
@@ -0,0 +1,109 @@
+// This is f#@%ing awsome: https://rss2json.com/
+
+const CNN   = "https://api.rss2json.com/v1/api.json?rss_url=http%3A%2F%2Frss.cnn.com%2Frss%2Fcnn_allpolitics.rss";
+const FOX   = "https://api.rss2json.com/v1/api.json?rss_url=http%3A%2F%2Ffeeds.foxnews.com%2Ffoxnews%2Fpolitics";
+const TIMES = "https://rss.nytimes.com/services/xml/rss/nyt/Politics.xml";
+const ONION = "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fpolitics.theonion.com%2Frss";
+
+const TIMES_RE  = /title>(.*)</g;
+
+const MAX_PER_ORG = 4;
+
+const JOKES = [
+    "Oof. Looks like you need to read a little more.",
+    "Well, maybe the news isn't as clear cut as I thought.",
+    "Not bad. Just goes to show how weird the news is.",
+    "Wow. The media is just as biased as they say everyone else is.",
+    "Hmph. Lucky guess."
+];
+
+let headlines = [];
+let loaded = 0;
+
+let current = null;
+let currentNum = 0;
+
+let correct = 0;
+
+$(document).ready(function() {
+    collectHeadlinesTest();
+
+    $("#cnn").click(function() {
+        submitResponse(0);
+    });
+
+    $("#fox").click(function() {
+        submitResponse(1);
+    });
+
+    $("#times").click(function() {
+        submitResponse(2);
+    });
+
+    $("#onion").click(function() {
+        submitResponse(3);
+    });
+
+    $("#false").hide();
+    $("#check").hide();
+    $("#end").hide();
+
+    $("#start").click(function() {
+        $("#start").hide();
+    });
+});
+
+function submitResponse(response) {
+    console.log(response);
+
+    if(current != null) {
+        if(response == current[1]) {
+            correct++;
+            $("#check").show();
+        } else {
+            $("#false").show();
+        }
+
+        setTimeout(clearQuestion, 2000);
+    }
+}
+
+function clearQuestion() {
+    $("#headline").hide();
+    $("#false").hide();
+    $("#check").hide();
+
+    setTimeout(nextQuestion, 50);
+}
+
+function nextQuestion() {
+    currentNum++;
+
+    if(currentNum < headlines.length) {
+        current = headlines[currentNum];
+
+        $("#headline").html(capitalizeWords(current[0]));
+        $("#headline").show();
+    } else {
+        let percent = Math.round((correct / currentNum) * 100);
+        end(percent);
+    }
+
+    $(".banner").html(currentNum + " / " + headlines.length);
+}
+
+function end(percent) {
+    $("#percent").html(percent + "%");
+    
+    let i = Math.floor(percent / 25);
+    $("#joke").html(JOKES[i]);
+
+    $("#end").show();
+}
+
+function capitalizeWords(str)
+{
+    return str.replace(/\w\S*/g, function(txt) {
+        return txt.charAt(0).toUpperCase() + txt.substr(1);
+    });
+}
