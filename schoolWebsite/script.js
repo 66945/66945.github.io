3fa5b92495f9b81da0836d321c02e2949c1d8512
66945 <54561572+66945@users.noreply.github.com>
2021-03-04 21:50:11 -0700

    Update script.js

--- schoolWebsite/script.js
+++ schoolWebsite/script.js
@@ -14,10 +14,10 @@ function scrolled() {
     }
 }
 
-// $.get("https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Ffch.psdschools.org%2Frss.xml", function(response) {
-//     console.log(response);
+ $.get("https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Ffch.psdschools.org%2Frss.xml", function(response) {
+     console.log(response);
     
-//     for (let i = 0; i < response["items"].length && i < 4; i++) {
-//         $("#news").append("<a href=\"" + response["items"][i]["link"] + "\">" + response["items"][i]["title"] + "</a><br><br>");
-//     }
-// });
+     for (let i = 0; i < response["items"].length && i < 4; i++) {
+         $("#news").append("<a href=\"" + response["items"][i]["link"] + "\">" + response["items"][i]["title"] + "</a><br><br>");
+     }
+ });
